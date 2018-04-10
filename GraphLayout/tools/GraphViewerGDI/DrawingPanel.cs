3407c6caa19c11eec4979e439508a6a92969b32d
LaFrank <laszlo.frank@gmail.com>
2018-04-10 13:31:16 +0200

    Revoked commit  e5fc769 "DrawReversibleFrame is broken in winforms, stop using it" because it works actually.

--- GraphLayout/tools/GraphViewerGDI/DrawingPanel.cs
+++ GraphLayout/tools/GraphViewerGDI/DrawingPanel.cs
@@ -39,271 +39,337 @@ using Color = System.Drawing.Color;
 using MouseButtons = System.Windows.Forms.MouseButtons;
 using P2 = Microsoft.Msagl.Core.Geometry.Point;
 
-namespace Microsoft.Msagl.GraphViewerGdi {
-    /// <summary>
-    /// this class serves as a drawing panel for GViewer
-    /// </summary>
-    internal class DrawingPanel : Control {
-        readonly Color rubberRectColor = Color.Black;
-        MouseButtons currentPressedButton;
-        GViewer gViewer;
+namespace Microsoft.Msagl.GraphViewerGdi
+{
+	/// <summary>
+	/// this class serves as a drawing panel for GViewer
+	/// </summary>
+	internal class DrawingPanel : Control
+	{
+		readonly Color rubberRectColor = Color.Green;
+		const FrameStyle RubberRectStyle = FrameStyle.Dashed;
+		MouseButtons currentPressedButton;
+		GViewer gViewer;
 
-        System.Drawing.Point mouseDownPoint;
+		System.Drawing.Point mouseDownPoint;
 
-        System.Drawing.Point mouseUpPoint;
-        P2 rubberLineEnd;
-        P2 rubberLineStart;
-        System.Drawing.Rectangle rubberRect;
-        const FrameStyle RubberRectStyle = FrameStyle.Dashed;
-        bool zoomWindow;
-        PlaneTransformation mouseDownTransform;
-
-        internal GViewer GViewer {
-            private get { return gViewer; }
-            set { gViewer = value; }
-        }
-
-    
-        DraggingMode MouseDraggingMode {
-            get {
-                if (gViewer.panButton.Pushed)
-                    return DraggingMode.Pan;
-                if (gViewer.windowZoomButton.Pushed)
-                    return DraggingMode.WindowZoom;
-                return DraggingMode.Default;
-            }
-        }
-
-        bool DrawingRubberEdge { get; set; }
-
-        P2 RubberLineEnd {
-            get { return rubberLineEnd; }
-            set {
-                if (DrawingRubberEdge)
-                    Invalidate(CreateRectForRubberEdge());
-                rubberLineEnd = value;
-            }
-        }
-
-        EdgeGeometry CurrentRubberEdge { get; set; }
-
-        internal void SetDoubleBuffering() {
-            // the magic calls for invoking doublebuffering
-            SetStyle(ControlStyles.UserPaint, true);
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-        }
+		System.Drawing.Point mouseUpPoint;
+		P2 rubberLineEnd;
+		P2 rubberLineStart;
+		System.Drawing.Rectangle rubberRect;
+		bool zoomWindow;
+		PlaneTransformation mouseDownTransform;
+		bool NeedToEraseRubber { get; set; }
+		internal GViewer GViewer
+		{
+			private get { return gViewer; }
+			set { gViewer = value; }
+		}
 
 
-        protected override void OnPaint(PaintEventArgs e) {
+		DraggingMode MouseDraggingMode
+		{
+			get
+			{
+				if (gViewer.panButton.Pushed)
+					return DraggingMode.Pan;
+				if (gViewer.windowZoomButton.Pushed)
+					return DraggingMode.WindowZoom;
+				return DraggingMode.Default;
+			}
+		}
 
-            if (gViewer != null && gViewer.Graph != null && gViewer.Graph.GeometryGraph != null) {
-                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                e.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
-                gViewer.ProcessOnPaint(e.Graphics, null);
-            }
-            if (CurrentRubberEdge != null)
-                using (GraphicsPath gp = Draw.CreateGraphicsPath(CurrentRubberEdge.Curve))
-                using (var pen = new Pen(Brushes.Black, (float) GViewer.LineThicknessForEditing))
-                    e.Graphics.DrawPath(pen, gp);
+		bool DrawingRubberEdge { get; set; }
 
-            if (DrawingRubberEdge)
-                e.Graphics.DrawLine(new Pen(Brushes.Black, (float) GViewer.LineThicknessForEditing),
-                                    (float) rubberLineStart.X, (float) rubberLineStart.Y, (float) RubberLineEnd.X,
-                                    (float) RubberLineEnd.Y);
-            base.OnPaint(e); // Filippo Polo 13/11/07; if I don't do this, onpaint events won't be invoked
-            gViewer.RaisePaintEvent(e);
-        }
+		P2 RubberLineEnd
+		{
+			get { return rubberLineEnd; }
+			set
+			{
+				if (DrawingRubberEdge)
+					Invalidate(CreateRectForRubberEdge());
+				rubberLineEnd = value;
+			}
+		}
 
-        protected override void OnMouseDown(MouseEventArgs e) {
-            base.OnMouseDown(e);
-            MsaglMouseEventArgs iArgs = CreateMouseEventArgs(e);
-            gViewer.RaiseMouseDownEvent(iArgs);
-            if (!iArgs.Handled) {
-                currentPressedButton = e.Button;
-                if (currentPressedButton == MouseButtons.Left)
-                    if (ClientRectangle.Contains(PointToClient(MousePosition))) {
-                        mouseDownPoint = new Point(e.X, e.Y);
-                        if (MouseDraggingMode != DraggingMode.Pan)
-                            zoomWindow = true;
-                        else {
-                            mouseDownTransform = gViewer.Transform.Clone();
-                        }
-                    }
-            }
-        }
+		EdgeGeometry CurrentRubberEdge { get; set; }
+
+		internal void SetDoubleBuffering()
+		{
+			// the magic calls for invoking doublebuffering
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+		}
 
 
-        
-        protected override void OnMouseUp(MouseEventArgs args) {
-            base.OnMouseUp(args);
-            MsaglMouseEventArgs iArgs = CreateMouseEventArgs(args);
-            gViewer.RaiseMouseUpEvent(iArgs);
-            
-            if (!iArgs.Handled) {
-                if (gViewer.OriginalGraph != null && MouseDraggingMode == DraggingMode.WindowZoom) {
-                    var p = mouseDownPoint;
-                    double f = Math.Max(Math.Abs(p.X - args.X), Math.Abs(p.Y - args.Y))/GViewer.Dpi;
-                    if (f > gViewer.ZoomWindowThreshold && zoomWindow) {
-                        mouseUpPoint = new Point(args.X, args.Y);
-                        if (ClientRectangle.Contains(mouseUpPoint)) {
-                            //var r = GViewer.RectFromPoints(mouseDownPoint, mouseUpPoint);
-                            //r.Intersect(gViewer.DestRect);
-                            if (GViewer.ModifierKeyWasPressed() == false) {
-                              
-                                P2 p1 = gViewer.ScreenToSource(mouseDownPoint);
-                                P2 p2 = gViewer.ScreenToSource(mouseUpPoint);
-                                double sc = Math.Min(Width / Math.Abs(p1.X - p2.X),
-                                    Height / Math.Abs(p1.Y - p2.Y));
-                                P2 center = 0.5f*(p1 + p2);
-                                gViewer.SetTransformOnScaleAndCenter(sc, center);
-                                Invalidate();
-                            }
-                        }
-                    }
-                }
-            }
-            zoomWindow = false;
-        }
+		protected override void OnPaint(PaintEventArgs e)
+		{
 
-        /// <summary>
-        /// Set context menu strip
-        /// </summary>
-        /// <param name="contexMenuStrip"></param>
-        public void SetCms(ContextMenuStrip contexMenuStrip) {
-            MouseClick +=
-                delegate(object sender, MouseEventArgs e) {
-                    if (e.Button == MouseButtons.Right) {
-                        var newE = new MouseEventArgs(
-                            MouseButtons.None,
-                            e.Clicks,
-                            e.X,
-                            e.Y,
-                            e.Delta);
+			if (gViewer != null && gViewer.Graph != null && gViewer.Graph.GeometryGraph != null)
+			{
+				e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+				e.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+				gViewer.ProcessOnPaint(e.Graphics, null);
+			}
+			if (CurrentRubberEdge != null)
+				using (GraphicsPath gp = Draw.CreateGraphicsPath(CurrentRubberEdge.Curve))
+				using (var pen = new Pen(Brushes.Black, (float)GViewer.LineThicknessForEditing))
+					e.Graphics.DrawPath(pen, gp);
 
-                        OnMouseMove(newE);
+			if (DrawingRubberEdge)
+				e.Graphics.DrawLine(new Pen(Brushes.Black, (float)GViewer.LineThicknessForEditing),
+														(float)rubberLineStart.X, (float)rubberLineStart.Y, (float)RubberLineEnd.X,
+														(float)RubberLineEnd.Y);
+			base.OnPaint(e); // Filippo Polo 13/11/07; if I don't do this, onpaint events won't be invoked
+			gViewer.RaisePaintEvent(e);
+		}
 
-                        contexMenuStrip.Show(this, e.X, e.Y);
-                    }
-                };
-        }
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			base.OnMouseDown(e);
+			MsaglMouseEventArgs iArgs = CreateMouseEventArgs(e);
+			gViewer.RaiseMouseDownEvent(iArgs);
+			if (!iArgs.Handled)
+			{
+				currentPressedButton = e.Button;
+				if (currentPressedButton == MouseButtons.Left)
+					if (ClientRectangle.Contains(PointToClient(MousePosition)))
+					{
+						mouseDownPoint = new Point(e.X, e.Y);
+						if (MouseDraggingMode != DraggingMode.Pan)
+							zoomWindow = true;
+						else
+						{
+							mouseDownTransform = gViewer.Transform.Clone();
+						}
+					}
+			}
+		}
 
-        protected override void OnMouseMove(MouseEventArgs args) {
-            MsaglMouseEventArgs iArgs = CreateMouseEventArgs(args);
-            gViewer.RaiseMouseMoveEvent(iArgs);
-            gViewer.RaiseRegularMouseMove(args);
-            if (!iArgs.Handled) {
-                if (gViewer.Graph != null) {
-                    SetCursor(args);
-                    if (MouseDraggingMode == DraggingMode.Pan)
-                        ProcessPan(args);
-                    else if (zoomWindow)
-                    {
-                        //the user is holding the left button, do nothing
-                    }
-                    else
-                        HitIfBbNodeIsNotNull(args);
-                }
-            }
-        }
+		void DrawXorFrame()
+		{
+			ControlPaint.DrawReversibleFrame(rubberRect, rubberRectColor, RubberRectStyle);
+			NeedToEraseRubber = !NeedToEraseRubber;
+		}
+
+		void DrawZoomWindow(MouseEventArgs args)
+		{
+			mouseUpPoint.X = args.X;
+			mouseUpPoint.Y = args.Y;
+
+			if (NeedToEraseRubber)
+				DrawXorFrame();
+
+			if (ClientRectangle.Contains(PointToClient(MousePosition)))
+			{
+				rubberRect = GViewer.RectFromPoints(PointToScreen(mouseDownPoint), PointToScreen(mouseUpPoint));
+				DrawXorFrame();
+			}
+		}
+
+		protected override void OnMouseUp(MouseEventArgs args)
+		{
+			base.OnMouseUp(args);
+			MsaglMouseEventArgs iArgs = CreateMouseEventArgs(args);
+			gViewer.RaiseMouseUpEvent(iArgs);
+			if (NeedToEraseRubber) DrawXorFrame();
+			if (!iArgs.Handled)
+			{
+				if (gViewer.OriginalGraph != null && MouseDraggingMode == DraggingMode.WindowZoom)
+				{
+					var p = mouseDownPoint;
+					double f = Math.Max(Math.Abs(p.X - args.X), Math.Abs(p.Y - args.Y)) / GViewer.Dpi;
+					if (f > gViewer.ZoomWindowThreshold && zoomWindow)
+					{
+						mouseUpPoint = new Point(args.X, args.Y);
+						if (ClientRectangle.Contains(mouseUpPoint))
+						{
+							//var r = GViewer.RectFromPoints(mouseDownPoint, mouseUpPoint);
+							//r.Intersect(gViewer.DestRect);
+							if (GViewer.ModifierKeyWasPressed() == false)
+							{
+
+								P2 p1 = gViewer.ScreenToSource(mouseDownPoint);
+								P2 p2 = gViewer.ScreenToSource(mouseUpPoint);
+								double sc = Math.Min(Width / Math.Abs(p1.X - p2.X),
+										Height / Math.Abs(p1.Y - p2.Y));
+								P2 center = 0.5f * (p1 + p2);
+								gViewer.SetTransformOnScaleAndCenter(sc, center);
+								Invalidate();
+							}
+						}
+					}
+				}
+			}
+			zoomWindow = false;
+		}
+
+		/// <summary>
+		/// Set context menu strip
+		/// </summary>
+		/// <param name="contexMenuStrip"></param>
+		public void SetCms(ContextMenuStrip contexMenuStrip)
+		{
+			MouseClick +=
+					delegate (object sender, MouseEventArgs e)
+					{
+						if (e.Button == MouseButtons.Right)
+						{
+							var newE = new MouseEventArgs(
+												MouseButtons.None,
+												e.Clicks,
+												e.X,
+												e.Y,
+												e.Delta);
+
+							OnMouseMove(newE);
+
+							contexMenuStrip.Show(this, e.X, e.Y);
+						}
+					};
+		}
+
+		protected override void OnMouseMove(MouseEventArgs args)
+		{
+			MsaglMouseEventArgs iArgs = CreateMouseEventArgs(args);
+			gViewer.RaiseMouseMoveEvent(iArgs);
+			gViewer.RaiseRegularMouseMove(args);
+			if (!iArgs.Handled)
+			{
+				if (gViewer.Graph != null)
+				{
+					SetCursor(args);
+					if (MouseDraggingMode == DraggingMode.Pan)
+						ProcessPan(args);
+					else if (zoomWindow)
+					{
+						//the user is holding the left button, do nothing
+						DrawZoomWindow(args);
+					}
+					else
+						HitIfBbNodeIsNotNull(args);
+				}
+			}
+		}
 
 
-        void HitIfBbNodeIsNotNull(MouseEventArgs args) {
-            if (gViewer.DGraph != null && gViewer.BbNode != null)
-                gViewer.Hit(args);
-        }
+		void HitIfBbNodeIsNotNull(MouseEventArgs args)
+		{
+			if (gViewer.DGraph != null && gViewer.BbNode != null)
+				gViewer.Hit(args);
+		}
 
 
-        static MsaglMouseEventArgs CreateMouseEventArgs(MouseEventArgs args) {
-            return new ViewerMouseEventArgs(args);
-        }
+		static MsaglMouseEventArgs CreateMouseEventArgs(MouseEventArgs args)
+		{
+			return new ViewerMouseEventArgs(args);
+		}
 
 
-        void SetCursor(MouseEventArgs args) {
-            Cursor cur;
-            if (MouseDraggingMode == DraggingMode.Pan) {
-                cur = args.Button == MouseButtons.Left
-                          ? gViewer.panGrabCursor
-                          : gViewer.panOpenCursor;
-            } else
-                cur = gViewer.originalCursor;
+		void SetCursor(MouseEventArgs args)
+		{
+			Cursor cur;
+			if (MouseDraggingMode == DraggingMode.Pan)
+			{
+				cur = args.Button == MouseButtons.Left
+									? gViewer.panGrabCursor
+									: gViewer.panOpenCursor;
+			}
+			else
+				cur = gViewer.originalCursor;
 
-            if (cur != null)
-                Cursor = cur;
-        }
-
-        
-        void ProcessPan(MouseEventArgs args) {
-            if (ClientRectangle.Contains(args.X, args.Y)) {
-                if (args.Button == MouseButtons.Left) {
-                    if (mouseDownTransform != null) {
-                        gViewer.Transform[0, 2] = mouseDownTransform[0, 2] + args.X - mouseDownPoint.X;
-                        gViewer.Transform[1, 2] = mouseDownTransform[1, 2] + args.Y - mouseDownPoint.Y;
-                    }
-                    gViewer.Invalidate();
-                } else
-                    GViewer.Hit(args);
-            }
-        }
-
-        protected override void OnKeyUp(KeyEventArgs e) {
-            gViewer.OnKey(e);
-            base.OnKeyUp(e);
-        }
+			if (cur != null)
+				Cursor = cur;
+		}
 
 
-        internal void DrawRubberLine(MsaglMouseEventArgs args) {
-            RubberLineEnd = gViewer.ScreenToSource(new Point(args.X, args.Y));
-            DrawRubberLineWithKnownEnd();
-        }
+		void ProcessPan(MouseEventArgs args)
+		{
+			if (ClientRectangle.Contains(args.X, args.Y))
+			{
+				if (args.Button == MouseButtons.Left)
+				{
+					if (mouseDownTransform != null)
+					{
+						gViewer.Transform[0, 2] = mouseDownTransform[0, 2] + args.X - mouseDownPoint.X;
+						gViewer.Transform[1, 2] = mouseDownTransform[1, 2] + args.Y - mouseDownPoint.Y;
+					}
+					gViewer.Invalidate();
+				}
+				else
+					GViewer.Hit(args);
+			}
+		}
 
-        internal void DrawRubberLine(P2 point) {
-            RubberLineEnd = point;
-            DrawRubberLineWithKnownEnd();
-        }
+		protected override void OnKeyUp(KeyEventArgs e)
+		{
+			gViewer.OnKey(e);
+			base.OnKeyUp(e);
+		}
 
-        void DrawRubberLineWithKnownEnd() {
-            DrawingRubberEdge = true;
-            Invalidate(CreateRectForRubberEdge());
-        }
 
-        Rectangle CreateRectForRubberEdge() {
-            var rect = new BBox(rubberLineStart, RubberLineEnd);
-            double w = gViewer.LineThicknessForEditing;
-            var del = new P2(-w, w);
-            rect.Add(rect.LeftTop + del);
-            rect.Add(rect.RightBottom - del);
-            return GViewer.CreateScreenRectFromTwoCornersInTheSource(rect.LeftTop, rect.RightBottom);
-        }
+		internal void DrawRubberLine(MsaglMouseEventArgs args)
+		{
+			RubberLineEnd = gViewer.ScreenToSource(new Point(args.X, args.Y));
+			DrawRubberLineWithKnownEnd();
+		}
 
-        internal void StopDrawRubberLine() {
-            DrawingRubberEdge = false;
-            Invalidate(CreateRectForRubberEdge());
-        }
+		internal void DrawRubberLine(P2 point)
+		{
+			RubberLineEnd = point;
+			DrawRubberLineWithKnownEnd();
+		}
 
-        internal void MarkTheStartOfRubberLine(P2 point) {
-            rubberLineStart = point;
-        }
+		void DrawRubberLineWithKnownEnd()
+		{
+			DrawingRubberEdge = true;
+			Invalidate(CreateRectForRubberEdge());
+		}
 
-        internal void DrawRubberEdge(EdgeGeometry edgeGeometry) {
-            BBox rectToInvalidate = edgeGeometry.BoundingBox;
-            if (CurrentRubberEdge != null) {
-                BBox b = CurrentRubberEdge.BoundingBox;
-                rectToInvalidate.Add(b);
-            }
-            CurrentRubberEdge = edgeGeometry;
-            GViewer.Invalidate(GViewer.CreateScreenRectFromTwoCornersInTheSource(rectToInvalidate.LeftTop,
-                                                                                 rectToInvalidate.RightBottom));
-        }
+		Rectangle CreateRectForRubberEdge()
+		{
+			var rect = new BBox(rubberLineStart, RubberLineEnd);
+			double w = gViewer.LineThicknessForEditing;
+			var del = new P2(-w, w);
+			rect.Add(rect.LeftTop + del);
+			rect.Add(rect.RightBottom - del);
+			return GViewer.CreateScreenRectFromTwoCornersInTheSource(rect.LeftTop, rect.RightBottom);
+		}
 
-        internal void StopDrawingRubberEdge() {
-            if (CurrentRubberEdge != null)
-                GViewer.Invalidate(
-                    GViewer.CreateScreenRectFromTwoCornersInTheSource(
-                        CurrentRubberEdge.BoundingBox.LeftTop,
-                        CurrentRubberEdge.BoundingBox.RightBottom));
+		internal void StopDrawRubberLine()
+		{
+			DrawingRubberEdge = false;
+			Invalidate(CreateRectForRubberEdge());
+		}
 
-            CurrentRubberEdge = null;
-        }
-    }
+		internal void MarkTheStartOfRubberLine(P2 point)
+		{
+			rubberLineStart = point;
+		}
+
+		internal void DrawRubberEdge(EdgeGeometry edgeGeometry)
+		{
+			BBox rectToInvalidate = edgeGeometry.BoundingBox;
+			if (CurrentRubberEdge != null)
+			{
+				BBox b = CurrentRubberEdge.BoundingBox;
+				rectToInvalidate.Add(b);
+			}
+			CurrentRubberEdge = edgeGeometry;
+			GViewer.Invalidate(GViewer.CreateScreenRectFromTwoCornersInTheSource(rectToInvalidate.LeftTop,
+																																					 rectToInvalidate.RightBottom));
+		}
+
+		internal void StopDrawingRubberEdge()
+		{
+			if (CurrentRubberEdge != null)
+				GViewer.Invalidate(
+						GViewer.CreateScreenRectFromTwoCornersInTheSource(
+								CurrentRubberEdge.BoundingBox.LeftTop,
+								CurrentRubberEdge.BoundingBox.RightBottom));
+
+			CurrentRubberEdge = null;
+		}
+	}
 }
