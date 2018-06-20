6320e669bb0c84512d53890f6bf1318cd6746470
Saveen Reddy <saveenr@live.com>
2018-06-19 20:00:43 -0700

    Updated nuget packages (#168)
    * Updated nuget packages
    
    * Updated nuget packages

--- GraphLayout/NuGet/build_nuget_packages.ps1
+++ GraphLayout/NuGet/build_nuget_packages.ps1
@@ -4,5 +4,5 @@ Push-Location $PSScriptRoot
 nuget pack Microsoft.Msagl.nuspec
 nuget pack Microsoft.Msagl.Drawing.nuspec
 nuget pack Microsoft.Msagl.GraphViewerGDI.nuspec
-nuget pack Microsoft.Msagl.WpfGraphControl.nuspec
+#nuget pack Microsoft.Msagl.WpfGraphControl.nuspec
 Pop-Location
