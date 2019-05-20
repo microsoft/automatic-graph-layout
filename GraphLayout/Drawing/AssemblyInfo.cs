e8140619e38c993594a5c2b1826be9501d98b88d
Filippo Polo <filippo@shpsoftware.it>
2019-05-20 08:58:18 +0200

    Changes to enable building Drawing for .NET Standard.

--- GraphLayout/Drawing/AssemblyInfo.cs
+++ GraphLayout/Drawing/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if !NETCORE
 [assembly: AssemblyTitle("Drawing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -15,6 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © MS 2006")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
