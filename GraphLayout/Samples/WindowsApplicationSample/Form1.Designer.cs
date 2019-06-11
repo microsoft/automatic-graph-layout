d033399920c28e9780c791547ea017d1ba4807a3
Filippo Polo <filippo@shpsoftware.it>
2019-06-11 15:58:21 +0200

    Added clusters examples to WindowsApplicationSample.

--- GraphLayout/Samples/WindowsApplicationSample/Form1.Designer.cs
+++ GraphLayout/Samples/WindowsApplicationSample/Form1.Designer.cs
@@ -28,6 +28,7 @@ namespace WindowsApplicationSample
     /// </summary>
     private void InitializeComponent()
     {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -36,14 +37,15 @@ namespace WindowsApplicationSample
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
             this.recalculateLayoutButton = new System.Windows.Forms.Button();
             this.ShowObjectsInTheLastRect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Location = new System.Drawing.Point(216, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(267, 23);
+            this.button1.Size = new System.Drawing.Size(259, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Create a graph, lay it out, and display in the viewer";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,7 +55,7 @@ namespace WindowsApplicationSample
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label1.Location = new System.Drawing.Point(287, 7);
+            this.label1.Location = new System.Drawing.Point(4, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 13);
             this.label1.TabIndex = 3;
@@ -67,10 +69,10 @@ namespace WindowsApplicationSample
             this.panel1.Controls.Add(this.gViewer);
             this.panel1.Controls.Add(this.splitter1);
             this.panel1.Controls.Add(this.propertyGrid1);
-            this.panel1.Location = new System.Drawing.Point(1, 29);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(1, 51);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(972, 505);
+            this.panel1.Size = new System.Drawing.Size(972, 483);
             this.panel1.TabIndex = 4;
             // 
             // gViewer
@@ -82,6 +84,7 @@ namespace WindowsApplicationSample
             this.gViewer.BuildHitTree = true;
             this.gViewer.CurrentLayoutMethod = Microsoft.Msagl.GraphViewerGdi.LayoutMethod.UseSettingsOfTheGraph;
             this.gViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gViewer.EdgeInsertButtonVisible = true;
             this.gViewer.FileName = "";
             this.gViewer.ForwardEnabled = false;
             this.gViewer.Graph = null;
@@ -102,10 +105,12 @@ namespace WindowsApplicationSample
             this.gViewer.SaveButtonVisible = true;
             this.gViewer.SaveGraphButtonVisible = true;
             this.gViewer.SaveInVectorFormatEnabled = true;
-            this.gViewer.Size = new System.Drawing.Size(674, 505);
+            this.gViewer.Size = new System.Drawing.Size(674, 483);
             this.gViewer.TabIndex = 3;
             this.gViewer.TightOffsetForRouting = 0.125D;
             this.gViewer.ToolBarIsVisible = true;
+            this.gViewer.Transform = ((Microsoft.Msagl.Core.Geometry.Curves.PlaneTransformation)(resources.GetObject("gViewer.Transform")));
+            this.gViewer.UndoRedoButtonsVisible = true;
             this.gViewer.WindowZoomButtonPressed = false;
             this.gViewer.ZoomF = 1D;
             this.gViewer.ZoomWindowThreshold = 0.05D;
@@ -115,7 +120,7 @@ namespace WindowsApplicationSample
             this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
             this.splitter1.Location = new System.Drawing.Point(674, 0);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 505);
+            this.splitter1.Size = new System.Drawing.Size(3, 483);
             this.splitter1.TabIndex = 2;
             this.splitter1.TabStop = false;
             // 
@@ -124,12 +129,12 @@ namespace WindowsApplicationSample
             this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Right;
             this.propertyGrid1.Location = new System.Drawing.Point(677, 0);
             this.propertyGrid1.Name = "propertyGrid1";
-            this.propertyGrid1.Size = new System.Drawing.Size(295, 505);
+            this.propertyGrid1.Size = new System.Drawing.Size(295, 483);
             this.propertyGrid1.TabIndex = 1;
             // 
             // recalculateLayoutButton
             // 
-            this.recalculateLayoutButton.Location = new System.Drawing.Point(740, 1);
+            this.recalculateLayoutButton.Location = new System.Drawing.Point(664, 7);
             this.recalculateLayoutButton.Name = "recalculateLayoutButton";
             this.recalculateLayoutButton.Size = new System.Drawing.Size(193, 23);
             this.recalculateLayoutButton.TabIndex = 5;
@@ -140,7 +145,7 @@ namespace WindowsApplicationSample
             // ShowObjectsInTheLastRect
             // 
             this.ShowObjectsInTheLastRect.AutoSize = true;
-            this.ShowObjectsInTheLastRect.Location = new System.Drawing.Point(441, 0);
+            this.ShowObjectsInTheLastRect.Location = new System.Drawing.Point(481, 7);
             this.ShowObjectsInTheLastRect.Name = "ShowObjectsInTheLastRect";
             this.ShowObjectsInTheLastRect.Size = new System.Drawing.Size(177, 23);
             this.ShowObjectsInTheLastRect.TabIndex = 6;
@@ -148,11 +153,27 @@ namespace WindowsApplicationSample
             this.ShowObjectsInTheLastRect.UseVisualStyleBackColor = true;
             this.ShowObjectsInTheLastRect.Click += new System.EventHandler(this.ShowObjectsInTheLastRectClick);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "graph",
+            "clusters",
+            "more clusters",
+            "more clusters (cluster-to-cluster edges only)",
+            "more clusters (horizontal)",
+            "more clusters (horizontal, cluster-to-cluster edges only)"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(970, 532);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.ShowObjectsInTheLastRect);
             this.Controls.Add(this.recalculateLayoutButton);
             this.Controls.Add(this.panel1);
@@ -176,7 +197,7 @@ namespace WindowsApplicationSample
     private System.Windows.Forms.Splitter splitter1;
       private Microsoft.Msagl.GraphViewerGdi.GViewer gViewer;
       private System.Windows.Forms.Button ShowObjectsInTheLastRect;
-
-  }
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
 }
 
--- GraphLayout/Samples/WindowsApplicationSample/Form1.cs
+++ GraphLayout/Samples/WindowsApplicationSample/Form1.cs
@@ -12,8 +12,6 @@ using Microsoft.Msagl.Layout.MDS;
 using Color = Microsoft.Msagl.Drawing.Color;
 using Label = Microsoft.Msagl.Drawing.Label;
 using MouseButtons = System.Windows.Forms.MouseButtons;
-using Node = Microsoft.Msagl.Core.Layout.Node;
-using Point = Microsoft.Msagl.Core.Geometry.Point;
 
 namespace WindowsApplicationSample {
     public partial class Form1 : Form {
@@ -50,7 +48,7 @@ namespace WindowsApplicationSample {
             }
             if (viewerEntityCorrespondingToLabelToChange == null) return;
             var rect = labelToChange.BoundingBox;
-            var font = new Font(labelToChange.FontName, (int) labelToChange.FontSize);
+            var font = new Font(labelToChange.FontName, (int)labelToChange.FontSize);
             double width;
             double height;
             StringMeasure.MeasureWithFont(labelToChange.Text, font, out width, out height);
@@ -71,11 +69,11 @@ namespace WindowsApplicationSample {
         }
 
         void WaMouseDown(object sender, MouseEventArgs e) {
-            if(e.Button==MouseButtons.Left)
+            if (e.Button == MouseButtons.Left)
                 myMouseDownPoint = e.Location;
         }
 
-        readonly Dictionary<object, Color> draggedObjectOriginalColors=new Dictionary<object, Color>();
+        readonly Dictionary<object, Color> draggedObjectOriginalColors = new Dictionary<object, Color>();
         System.Drawing.Point myMouseDownPoint;
         System.Drawing.Point myMouseUpPoint;
 
@@ -110,7 +108,7 @@ namespace WindowsApplicationSample {
             //DisplayGeometryGraph.SetShowFunctions();
 #endif
 
-          /////  CreateGraph();
+            /////  CreateGraph();
         }
 
         void GViewerObjectUnderMouseCursorChanged(object sender, ObjectUnderMouseCursorChangedEventArgs e) {
@@ -125,20 +123,22 @@ namespace WindowsApplicationSample {
             if (gViewer.ObjectUnderMouseCursor == null) {
                 label1.Text = "No object under the mouse";
                 gViewer.SetToolTip(toolTip1, "");
-            } else {
+            }
+            else {
                 selectedObject = gViewer.ObjectUnderMouseCursor.DrawingObject;
                 var edge = selectedObject as Edge;
                 if (edge != null) {
                     selectedObjectAttr = edge.Attr.Clone();
-                   edge.Attr.Color = Color.Blue;
+                    edge.Attr.Color = Color.Blue;
                     gViewer.Invalidate(e.NewObject);
 
-           //         here we can use e.Attr.Id or e.UserData to get back to the user data
+                    //         here we can use e.Attr.Id or e.UserData to get back to the user data
                     gViewer.SetToolTip(toolTip1, String.Format("edge from {0} to {1}", edge.Source, edge.Target));
-                } else if (selectedObject is Microsoft.Msagl.Drawing.Node) {
+                }
+                else if (selectedObject is Microsoft.Msagl.Drawing.Node) {
                     selectedObjectAttr = (gViewer.SelectedObject as Microsoft.Msagl.Drawing.Node).Attr.Clone();
                     (selectedObject as Microsoft.Msagl.Drawing.Node).Attr.Color = Color.Green;
-                 // //   here you can use e.Attr.Id to get back to your data
+                    // //   here you can use e.Attr.Id to get back to your data
                     gViewer.SetToolTip(toolTip1,
                                        String.Format("node {0}",
                                                      (selectedObject as Microsoft.Msagl.Drawing.Node).Attr.Id));
@@ -146,13 +146,13 @@ namespace WindowsApplicationSample {
                 }
                 label1.Text = selectedObject.ToString();
             }
-           
+
         }
 
         void RestoreSelectedObjAttr() {
             var edge = selectedObject as Edge;
             if (edge != null) {
-                edge.Attr = (EdgeAttr) selectedObjectAttr;
+                edge.Attr = (EdgeAttr)selectedObjectAttr;
             }
             else {
                 var node = selectedObject as Microsoft.Msagl.Drawing.Node;
@@ -160,16 +160,136 @@ namespace WindowsApplicationSample {
                     node.Attr = (NodeAttr)selectedObjectAttr;
 
             }
-            
+
         }
 
 
         void Button1Click(object sender, EventArgs e) {
-            CreateGraph();
+            switch (comboBox1.SelectedIndex) {
+                case 1:
+                    CreateGraphClustersSmall();
+                    break;
+                case 2:
+                    CreateGraphClustersBig(false, false);
+                    break;
+                case 3:
+                    CreateGraphClustersBig(true, false);
+                    break;
+                case 4:
+                    CreateGraphClustersBig(false, true);
+                    break;
+                case 5:
+                    CreateGraphClustersBig(true, true);
+                    break;
+                default:
+                    CreateGraph();
+                    break;
+            }
         }
 
         Label labelToChange;
         IViewerObject viewerEntityCorrespondingToLabelToChange;
+
+        void CreateGraphClustersSmall() {
+            Graph graph = new Graph("clusters");
+            var receivers_growth = new Subgraph("Receivers_growth");
+            var receiver0_growth = new Node("Receiver0_growth");
+            receivers_growth.AddNode(receiver0_growth);
+            var receivers_control = new Subgraph("Receivers_control");
+            var receiver0_control = new Node("Receiver0_control");
+            receivers_control.AddNode(receiver0_control);
+            var receivers = new Subgraph("Receivers");
+            var receiver0 = new Node("Receiver0");
+            receivers.AddNode(receiver0);
+            graph.RootSubgraph.AddSubgraph(receivers_growth);
+            graph.AddNode(receiver0_growth);
+            graph.RootSubgraph.AddSubgraph(receivers_control);
+            graph.AddNode(receiver0_control);
+            graph.RootSubgraph.AddSubgraph(receivers);
+            graph.AddNode(receiver0);
+            graph.AddEdge(receivers_growth.Id, receivers_control.Id);
+            graph.AddEdge(receiver0_control.Id, receiver0.Id);
+            graph.AddEdge(receiver0_growth.Id, receiver0.Id);
+            gViewer.Graph = graph;
+            this.propertyGrid1.SelectedObject = graph;
+        }
+
+        void CreateGraphClustersBig(bool enforceClusterEdgesOnly, bool horizontal) {
+            Graph graph = new Graph("clusters big");
+
+            void addCluster(string clusterId, params string[] nodeIds) {
+                if (nodeIds.Length == 0)
+                    nodeIds = new string[] { clusterId };
+                var cluster = new Subgraph(clusterId);
+                foreach (var nodeId in nodeIds) {
+                    var node = new Node(clusterId + "." + nodeId);
+                    node.LabelText = nodeId;
+                    graph.AddNode(node);
+                    cluster.AddNode(node);
+                }
+                graph.RootSubgraph.AddSubgraph(cluster);
+            }
+
+            addCluster("Arabinose");
+            addCluster("Arabinose_control");
+            addCluster("Arabinose_growth");
+            addCluster("Auto");
+            addCluster("Auto_control");
+            addCluster("Auto_growth");
+            addCluster("Degrader");
+            addCluster("Degrader_control");
+            addCluster("Degrader_growth");
+            addCluster("Receivers", "Receiver0", "Receiver1", "Receiver2", "Receiver3");
+            addCluster("Receivers_control", "Receiver0_control", "Receiver1_control", "Receiver2_control", "Receiver3_control");
+            addCluster("Receivers_growth", "Receiver0_growth", "Receiver1_growth", "Receiver2_growth", "Receiver3_growth");
+            addCluster("Relays", "Relay1", "Relay2");
+            addCluster("Relays_control", "Relay1_control", "Relay2_control");
+            addCluster("Relays_growth", "Relay1_growth", "Relay2_growth");
+            addCluster("Standard");
+            addCluster("Standard_control");
+            addCluster("Standard_growth");
+
+            void addEdge(string sourceCluster, string sourceNode, string targetCluster, string targetNode) {
+                if (enforceClusterEdgesOnly)
+                    graph.AddEdge(sourceCluster, targetCluster);
+                else
+                    graph.AddEdge(sourceNode == null ? sourceCluster : (sourceCluster + "." + sourceNode), targetNode == null ? targetCluster : (targetCluster + "." + targetNode));
+            }
+
+            addEdge("Arabinose_control", "Arabinose_control", "Arabinose", "Arabinose");
+            addEdge("Arabinose_control", "Arabinose_control", "Degrader_control", "Degrader_control");
+            addEdge("Arabinose_growth", "Arabinose_growth", "Arabinose_control", "Arabinose_control");
+            addEdge("Auto", null, "Standard", null);
+            addEdge("Auto_control", "Auto_control", "Auto", "Auto");
+            addEdge("Auto_control", null, "Standard_control", null);
+            addEdge("Auto_growth", "Auto_growth", "Auto_control", "Auto_control");
+            addEdge("Degrader_control", "Degrader_control", "Degrader", "Degrader");
+            addEdge("Degrader_growth", "Degrader_growth", "Degrader_control", "Degrader_control");
+            addEdge("Receivers", null, "Relays", null);
+            addEdge("Receivers_control", null, "Relays_control", null);
+            addEdge("Receivers_control", "Receiver0_control", "Receivers", "Receiver0");
+            addEdge("Receivers_control", "Receiver1_control", "Receivers", "Receiver1");
+            addEdge("Receivers_control", "Receiver2_control", "Receivers", "Receiver2");
+            addEdge("Receivers_control", "Receiver3_control", "Receivers", "Receiver3");
+            addEdge("Receivers_growth", "Receiver0_growth", "Receivers_control", "Receiver0_control");
+            addEdge("Receivers_growth", "Receiver1_growth", "Receivers_control", "Receiver1_control");
+            addEdge("Receivers_growth", "Receiver2_growth", "Receivers_control", "Receiver2_control");
+            addEdge("Receivers_growth", "Receiver3_growth", "Receivers_control", "Receiver3_control");
+            addEdge("Relays", null, "Degrader", null);
+            addEdge("Relays_control", "Relay1_control", "Relays", "Relay1");
+            addEdge("Relays_control", "Relay2_control", "Relays", "Relay2");
+            addEdge("Relays_growth", "Relay1_growth", "Relays_control", "Relay1_control");
+            addEdge("Relays_growth", "Relay2_growth", "Relays_control", "Relay2_control");
+            addEdge("Standard", null, "Receivers", null);
+            addEdge("Standard_control", null, "Receivers_control", null);
+            addEdge("Standard_growth", "Standard_growth", "Standard_control", "Standard_control");
+
+            if (horizontal)
+                (graph.LayoutAlgorithmSettings as SugiyamaLayoutSettings).Transformation = PlaneTransformation.Rotation(Math.PI / 2.0);
+
+            gViewer.Graph = graph;
+            this.propertyGrid1.SelectedObject = graph;
+        }
 
         void CreateGraph() {
 #if DEBUG
@@ -201,11 +321,11 @@ namespace WindowsApplicationSample {
 
             /*
               4->5
-5->7
-7->8
-8->22
-22->24
-*/
+    5->7
+    7->8
+    8->22
+    22->24
+    */
 
             graph.AddEdge("1", "2");
             graph.AddEdge("1", "3");
@@ -323,7 +443,7 @@ namespace WindowsApplicationSample {
             graph.AddEdge("29", "58");
             graph.AddEdge("5", "58");
             graph.AddEdge("47", "58");
-            
+
             //ChangeNodeSizes(graph);
 
             //var sls = graph.LayoutAlgorithmSettings as SugiyamaLayoutSettings;
@@ -337,8 +457,7 @@ namespace WindowsApplicationSample {
             this.propertyGrid1.SelectedObject = graph;
         }
 
-        private void StraightenEdge(Edge e, Graph graph)
-        {
+        private void StraightenEdge(Edge e, Graph graph) {
             graph.LayerConstraints.AddUpDownVerticalConstraint(e.SourceNode, e.TargetNode);
         }
 
@@ -347,19 +466,19 @@ namespace WindowsApplicationSample {
         }
 
 
-        bool MouseDownPointAndMouseUpPointsAreFarEnough()
-        {
+        bool MouseDownPointAndMouseUpPointsAreFarEnough() {
             double dx = myMouseDownPoint.X - myMouseUpPoint.X;
             double dy = myMouseDownPoint.Y - myMouseUpPoint.Y;
-            
-            return dx*dx + dy*dy >= 25; //so 5X5 pixels already give something
+
+            return dx * dx + dy * dy >= 25; //so 5X5 pixels already give something
         }
 
         void ShowObjectsInTheLastRectClick(object sender, EventArgs e) {
             string message;
-            if(gViewer.Graph==null) {
+            if (gViewer.Graph == null) {
                 message = "there is no graph";
-            }else {
+            }
+            else {
                 if (MouseDownPointAndMouseUpPointsAreFarEnough()) {
                     var p0 = gViewer.ScreenToSource(myMouseDownPoint);
                     var p1 = gViewer.ScreenToSource(myMouseUpPoint);
