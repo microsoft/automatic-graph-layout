7d42ecd036cb3f2c8485e80794a6ef03add81911
APlacuzzi <andrea.placuzzi@unibo.it>
2020-12-04 17:02:00 +0100

    SVG: export label as multi-line text

--- GraphLayout/Drawing/SvgGraphWriter.cs
+++ GraphLayout/Drawing/SvgGraphWriter.cs
@@ -11,6 +11,7 @@ using Microsoft.Msagl.Core.Geometry.Curves;
 using System.Linq;
 using Microsoft.Msagl.DebugHelpers;
 using LineSegment = Microsoft.Msagl.Core.Geometry.Curves.LineSegment;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.Msagl.Drawing
 {
@@ -160,16 +161,38 @@ namespace Microsoft.Msagl.Drawing
 
             var x = label.Center.X - label.Width / 2;
             var y = label.Center.Y + label.Height / (2 * yScaleAdjustment);
+            var fontSize = 16;
             WriteStartElement("text");
             WriteAttribute("x", x);
             WriteAttribute("y", y);
             WriteAttribute("font-family", "Arial");
-            WriteAttribute("font-size", "16");
+            WriteAttribute("font-size", fontSize);
             WriteAttribute("fill", MsaglColorToSvgColor(label.FontColor));
-            xmlWriter.WriteRaw(NodeSanitizer(label.Text));
+            WriteLabelText(label.Text, x, fontSize);
             WriteEndElement();
         }
 
+        private void WriteLabelText(string text, double xContainer, double fontSize) 
+        {
+            var endOfLine = new List<string> { "\r\n", "\r", "\n" };
+            var textLines = Regex.Split(NodeSanitizer(text), "(\r\n|\r|\n)").Where(it => !endOfLine.Contains(it)).ToList();
+            var isFirstLine = true;
+            textLines.ForEach(line => 
+            {
+                WriteStartElement("tspan");
+                WriteAttribute("x", xContainer);
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    WriteAttribute("dy", -1 * fontSize * (textLines.Count - 1));
+                }
+                else 
+                {
+                    WriteAttribute("dy", fontSize);
+                }
+                xmlWriter.WriteRaw(line);
+                WriteEndElement();
+            });
+        }
 
         protected string MsaglColorToSvgColor(Color color) {
             if (BlackAndWhite)
