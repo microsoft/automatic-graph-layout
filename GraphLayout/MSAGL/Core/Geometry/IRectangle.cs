48e91abdbb9b2803978de13b068e6a10d4160bba
Lev Nachmanson <levnach@hotmail.com>
2021-03-31 11:12:06 -0700

    in the middle
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ GraphLayout/MSAGL/Core/Geometry/IRectangle.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Msagl.Core.Geometry {
+    public interface IRectangle<P> {
+        void Add(P point);
+        void Add(IRectangle<P> rectangle);
+        bool Contains(P point);
+        bool Contains(IRectangle<P> rect);
+        IRectangle<P> Intersection(IRectangle<P> rectangle);
+        bool Intersects(IRectangle<P> rectangle);
+        IRectangle<P> Unite(IRectangle<P> b);
+        double Area { get; }
+    }
+}
