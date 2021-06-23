938c31310176fd9562d4a6d06aea02bf4306b6da
Lev Nachmanson <levnach@hotmail.com>
2021-06-23 15:07:34 -0700

    rename
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ GraphLayout/MSAGL/Core/Layout/EdgeConstraints.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Msagl.Core.Geometry;
+
+namespace Microsoft.Msagl.Core.Layout
+{
+    /// <summary>
+    /// Settings controlling how ideal edge lengths will be calculated for layouts that consider it.
+    /// </summary>
+    /// <remarks>
+    /// This is a struct so we can do a shallow copy without having to do a MemberwiseClone.
+    /// </remarks>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes")]
+    public struct EdgeConstraints
+    {
+        
+        /// <summary>
+        /// If not None, then direction separation constraints will be applied to all edges on InitializeLayout
+        /// </summary>
+        public Direction Direction { get; set; }
+
+        /// <summary>
+        /// Controls the separation used in Edge Constraints
+        /// </summary>
+        public double Separation
+        {
+            get { return constrainedEdgeSeparation; }
+            set { constrainedEdgeSeparation = value; }
+        }
+
+        internal double constrainedEdgeSeparation;
+
+        
+    }
+}
